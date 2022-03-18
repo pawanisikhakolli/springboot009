bb3d36d5dba4efe72c17a1871e5f26d53e3bb39c
Sikhakolli <v.pavani.sikhakolli@accenture.com>
2022-03-18 17:17:57 +0530

    pav

---
+++ src/main/java/com/example/springboot009/Springboot009Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot009Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot009Application.class, args);
+	}
+
+}
